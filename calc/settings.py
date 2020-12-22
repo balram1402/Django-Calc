10362ce87637dd9955aebaef2e4c27e629ffb068
Balram Potabatti <71968509+balram1402@users.noreply.github.com>
2020-12-22 21:31:19 +0530

    Update settings.py

--- calc/settings.py
+++ calc/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8+g_x814kz)^j74%9kql6f*)@d^lhv8i_&*4q5^c$%rhx1&m!n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
